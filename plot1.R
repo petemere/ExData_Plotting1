8a96013216196d39dccb3d3b857b28fec3a36b42
petemere <petemere@users.noreply.github.com>
2015-02-05 22:56:51 +1100

    First plot.

---
+++ plot1.R
@@ -0,0 +1,31 @@
+## Read the electricity power use data and produce a plot to match the relevant
+## example.
+
+## Open the data file.
+## Assuming data file is in current working directory.
+## setwd("C:/Users/mere_p/Desktop/PM/DS Specialisation/04 Exploratory Data Analysis/ExData_Plotting1")
+
+fileName <- 'household_power_consumption.txt'
+setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+setAs("character","myTime", function(from) strptime(from, format="%T"))
+
+powerData <- read.table(file = fileName, header = TRUE, sep = ';', 
+                        na.strings = '?', 
+                        colClasses = c('myDate', 'myTime', 
+                                       rep.int('numeric', 7)))
+## Keep only the dates of interest
+powerData <- powerData[powerData$Date >= as.Date('2007-02-01') & 
+                       powerData$Date <= as.Date('2007-02-02'), ]
+
+## Open a PNG device to save the plot as a PNG file
+png('plot1.png', width = 480, height = 480, bg = "transparent")
+
+## Create the plot.
+hist(x = powerData$Global_active_power, col = 'red', 
+     xlab = 'Global Active Power (kilowatts)', 
+     ylab = 'Frequency', 
+     main = 'Global Active Power',
+     mar = rep(0,4))
+
+## Close the display device
+dev.off()
